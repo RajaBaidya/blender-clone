ba23667d637545fb6aea54e65f7d944ff47a7dc4
RajaBaidya <blendervideo9@gmail.com>
2025-05-26 15:10:11 +0530

    new

--- src/page/feature.jsx
+++ src/page/feature.jsx
@@ -29,25 +29,45 @@ function Feature() {
         </div>
       </nav>
 
-      <div className="feature header-size-large header-align-x-center header-align-y-center is-container-overlap">
-        <div className="container">
-          <div className="feature-content">
-            <h1 className="display-4">Free Software Never Looked This Awesome</h1>
+          <div className="feature header-size-large header-align-x-center header-align-y-center is-container-overlap">
+            <div className="container">
+              <div className="feature-content">
+                <h1 className="display-4">Free Software Never Looked This Awesome</h1>
+              </div>
+            </div>
+            <div
+              className="feature-bg"
+              style={{
+                backgroundImage: "url(https://www.blender.org/wp-content/uploads/2017/11/features_rendering_02.jpg)",
+                backgroundPositionY: "50%",
+              }}
+            ></div>
+            <div
+              className="feature-overlay"
+              style={{ backgroundColor: "rgba(10,10,10,0.58)" }}
+            ></div>
+            <div className="feature-credits">pokedstudio.com</div>
           </div>
-        </div>
-        <div
-          className="feature-bg"
-          style={{
-            backgroundImage: "url(https://www.blender.org/wp-content/uploads/2017/11/features_rendering_02.jpg)",
-            backgroundPositionY: "50%",
-          }}
-        ></div>
-        <div
-          className="feature-overlay"
-          style={{ backgroundColor: "rgba(10,10,10,0.58)" }}
-        ></div>
-        <div className="feature-credits">pokedstudio.com</div>
-      </div>
+
+            <div className="container">
+                <div className="wp-block-group is-style-box is-layout-constrained wp-block-group-is-layout-constrained">
+                  <p>Blender is the free and open source 3D 
+                    creation suite. It supports the entirety 
+                    of the 3D pipeline—modeling, rigging, 
+                    animation, simulation, rendering, compositing
+                     and motion tracking, even video editing and 
+                     game asset creation. Advanced users employ 
+                     Blender’s API for Python scripting to customize 
+                     the application and write specialized tools; 
+                     often these are included in Blender’s future 
+                     releases. Blender is well suited to individuals
+                      and small studios who benefit from its unified
+                       pipeline and responsive development process. 
+                       Examples from many Blender-based projects are 
+                       available in the&nbsp;<a href="https://www.blender.org/features/">showcase</a>.</p>
+                </div>
+            </div>
+
     </div>
   );
 }
